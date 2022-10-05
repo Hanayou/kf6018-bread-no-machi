eba6aa5f75069281c05e4f7e2d6cc22f77e627ed
Hanayou <samuel.short@gmail.com>
2022-10-05 14:12:35 +0100

    convert modules to use local source files in place of CDN

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
-import Stats from 'https://cdn.jsdelivr.net/npm/three@0.145.0/examples/jsm/libs/stats.module.js';
+import * as THREE from 'modules/three.module.js';
+import { OrbitControls } from 'modules/OrbitControls.js';
+import Stats from 'modules/stats.module.js';
 
 console.log("Create the scene");
 var scene = new THREE.Scene();
